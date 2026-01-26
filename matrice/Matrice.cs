9509380bc963b99ece5072ea970f50e1ffa62370
hugorabelais <hugohamonic3.0@gmail.com>
2026-01-26 13:05:35 +0100

    matrice

---
+++ matrice/Matrice.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrice
+{
+    internal class Matrice
+    {
+        private int[,] tabMatrice;
+        private int ordre;
+
+        public Matrice(int pOrdre, int pMin, int pMax)
+        {
+            Random rand = new Random();
+            tabMatrice = new int[pOrdre,pOrdre];
+            int coefficient;
+            for (int i = 0; i < pOrdre; i++)
+            {
+                for (int j = 0; j < pOrdre; j++)
+                {
+                    coefficient = rand.Next(pMin, pMax);
+                    tabMatrice[i,j] = coefficient ;
+                }
+            }
+            ordre = pOrdre;
+        }
+
+        public Matrice(int[,] pTableau) 
+        {
+            tabMatrice = pTableau;
+            ordre = tabMatrice.GetLength(0);
+        }
+
+        public Matrice MultiplierParScalaire (int scalaire)
+        {
+            for (int i = 0; i < ordre ; i++)
+            {
+                for (int j = 0; j <= ordre ; j++)
+                {
+                    tabMatrice [i,j] *= scalaire ;
+                }
+            }
+            return this;
+        }
+
+        public override string ToString()
+        {
+            string resultat = string.Empty;
+            for (int i = 0; i < ordre; i++)
+            {
+                for (int j = 0; j < ordre; j++)
+                {
+                    resultat += tabMatrice[i,j].ToString()+ " ";
+                }
+                resultat += "\n";
+            }
+            return resultat;
+        }
+
+
+
+    }
+}
---
+++ matrice/test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrice
+{
+    static class TestCompte
+    {
+        public static void Main()
+        {
+            Matrice matrice1;
+            matrice1 = new Matrice (3, 0,9);
+            Console.WriteLine(matrice1);
+
+        }
+    }
+}
